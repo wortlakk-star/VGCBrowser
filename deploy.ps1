19088dc2b6bbbda375c2db4da3da7c8bce80739b
VGC Group <veltrixworldwide@gmail.com>
2026-06-06 09:32:45 -0700

    auto-sync 2026-06-06 09:32:45

--- deploy.ps1
+++ deploy.ps1
@@ -9,8 +9,9 @@ $ErrorActionPreference = 'Stop'
 $SSH_HOST = '212.85.28.201'
 $SSH_PORT = '65002'
 $SSH_USER = 'u469659181'
-# vgcbrowser.com is a Hostinger addon domain -> served from this folder:
+# vgcbrowser.com is a Hostinger addon domain -> served from these folders:
 $REMOTE   = 'domains/vgcbrowser.com/public_html/dl'
+$WEBROOT  = 'domains/vgcbrowser.com/public_html'
 
 $dir = Join-Path $PSScriptRoot 'release\nsis-web'
 if (-not (Test-Path $dir)) {
@@ -34,6 +35,13 @@ $files | ForEach-Object { Write-Host "  $_" }
 
 ssh -p $SSH_PORT -o StrictHostKeyChecking=accept-new "$SSH_USER@$SSH_HOST" "mkdir -p $REMOTE"
 scp -P $SSH_PORT $files "${SSH_USER}@${SSH_HOST}:$REMOTE/"
+
+# Also upload the landing page (its Download button auto-reads latest.yml)
+$landing = Join-Path $PSScriptRoot 'web\index.html'
+if (Test-Path $landing) {
+  Write-Host "Uploading landing page (web/index.html)" -ForegroundColor Cyan
+  scp -P $SSH_PORT $landing "${SSH_USER}@${SSH_HOST}:$WEBROOT/index.html"
+}
 
 if ($LASTEXITCODE -eq 0) {
   Write-Host ""
